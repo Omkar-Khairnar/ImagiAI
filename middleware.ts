72c7f3bddcb730f5d595524164348ae9f7631020
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-03-09 23:33:18 +0530

    routes bug fixed and env updated

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // // Routes that can be accessed while signed out
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
   // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/sign-in"]
-  ignoredRoutes:['/']
+ 
 
 });
 
